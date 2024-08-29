a0efe92ae1df2a4081615ee957104a39d61b3758
addysrii <149177246+addysrii@users.noreply.github.com>
2024-08-29 23:47:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import eventRoutes from './routes/eventsRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import path from 'path';
+import adminRoutes from "./routes/adminRoutes.js"
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -22,6 +23,8 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/api/events', eventRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes);
 app.use('/uploads/EventImages', express.static(path.join(__dirname, '/uploads/EventImages')));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
