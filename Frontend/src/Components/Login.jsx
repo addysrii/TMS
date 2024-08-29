2ed4c8b0359fd01b9bc5ed7cc4bc7287f7a53807
addysrii <149177246+addysrii@users.noreply.github.com>
2024-08-29 07:51:57 +0530

    Update Login.jsx

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const { data } = await axios.post('http://localhost:5001/api/users/login', { email, password });
+      const { data } = await axios.post('https://tms-backend2.onrender.com/api/users/login', { email, password });
       localStorage.setItem('token', data.token);
       navigate('/events'); // Navigate to dashboard or home page after login
     } catch (error) {
