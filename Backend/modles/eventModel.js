d43b3f4d1ade276de2a50fcb34e7869ef67bc5dc
addysrii <149177246+addysrii@users.noreply.github.com>
2024-08-30 11:27:02 +0530

    Update eventModel.js

--- Backend/modles/eventModel.js
+++ Backend/modles/eventModel.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 const eventSchema = new mongoose.Schema({
 title :{type: String, required:true},
 description:{type:String,required:true},
-date : {type:Date, required:true},
+startDate : {type:Date, required:true},
+endDate : {type:Date, required:true},
 location : { type:String, required:true},
 price : {type:Number, required:true},
 seats : {type:Number, required:true},
