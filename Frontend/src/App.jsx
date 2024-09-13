e4fd6ba9259035b12a89ba397251d95079fc4c5d
addysrii <149177246+addysrii@users.noreply.github.com>
2024-09-13 18:33:41 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,38 +1,40 @@
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Navigation from './Components/Navigation'
-import EventList from './Components/EventList'
+import './App.css';
+import Navigation from './Components/Navigation';
+import EventList from './Components/EventList';
 import Login from './Components/Login';
-import Dashboard from "./Components/Dashboard"
+import Dashboard from './Components/Dashboard';
 import Landing from './Components/Landing';
 import Register from './Components/Register';
 import ManageUsers from './Admin/ManageUsers';
-import ManageBookings from './Admin/ManageBookings'; 
+import ManageBookings from './Admin/ManageBookings';
 import ManageEvents from './Admin/ManageEvents';
-import AdminProfile from './Admin/AdminProfile';
 import AdminDashboard from './Admin/Dashboard';
-function App() {
-  
+import AdminCreateEvent from './Admin/AdminCreateEvent';
 
+function App() {
   return (
     <>
-<Router>
-      <Navigation />
-      <Routes>
+      <Router>
+        <Navigation />
+        <Routes>
+          {/* Define all your routes here */}
           <Route path="/" element={<Landing />} />
-        <Route path="/events" element={<EventList />} />
-            <Route path="/login" element={<Login />} />
-             <Route path="/register" element={<Register />} />
-             <Route path="/dashboard" element={<Dashboard/>} />
-              <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                <Route path="/admin/manageevents" element={<ManageEvents />} />
-         <Route path="/admin/managebookings" element={<ManageBookings />} />
-         <Route path="/admin/manageusers" element={<ManageUsers />} />
-      </Routes>
-    </Router>
+          <Route path="/events" element={<EventList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* Admin Routes */}
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/manageevents" element={<ManageEvents />} />
+          <Route path="/admin/managebookings" element={<ManageBookings />} />
+          <Route path="/admin/manageusers" element={<ManageUsers />} />
+          <Route path="/admin/createevent" element={<AdminCreateEvent />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
